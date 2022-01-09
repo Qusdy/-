9a626673b712c1d9151834b4dff114c9db7847ae
Albertlll <90716405+Albertlll@users.noreply.github.com>
2022-01-09 15:27:13 +0300

    Add files via upload

--- map_generation.py
+++ map_generation.py
@@ -5,6 +5,10 @@ from tree import Tree
 from mushrooms import TwoMushrooms, OneMushroom
 from random import randint
 from mana import Mana
+from rubin import Rubin
+from emerald import Emerald
+from diamond import Diamond
+
 
 def load_level(file):
     file = open(file, mode="rt", encoding="UTF-8")
@@ -50,30 +54,39 @@ def gen_map():
                 all_sprites.add(grass_sprite)
                 all_sprites.add(mushroom_sprite)
 
-            #
-            # else:
-            #     road_sprite = Road((i * 32, z * 32))
-            #     map_sprites.add(road_sprite)
-            #     all_sprites.add(road_sprite)
-
 
 def gen_mana():
     for i in range(100):
-        coords = (randint(13 * 32, LEVEL_WIGHT - 13 * 32), randint(13 * 32, LEVEL_WIGHT - 13 * 32))
-        print(coords)
+        coords = (randint(20 * 32, LEVEL_WIGHT - 20 * 32), randint(20 * 32, LEVEL_WIGHT - 20 * 32))
+
         mana = Mana((coords[0], coords[1]))
         mana_group.add(mana)
         all_sprites.add(mana)
 
 
+def gen_rubins():
+    for i in range(3):
+        coords = (randint(20 * 32, LEVEL_WIGHT - 20 * 32), randint(20 * 32, LEVEL_WIGHT - 20 * 32))
+        rubin = Rubin((coords[0], coords[1]))
+        crystal_group.add(rubin)
+        rubins_group.add(rubin)
+        all_sprites.add(rubin)
 
-# def add_forest():
-#     for i in range(0, len(level), 2):
-#         for j in range(0, 14, 2):
-#             level[i][j] = TREE
-#             level[i][-j] = TREE
-#             #
-#             level[j][i] = TREE
-#             level[-j][i] = TREE
-#     for i in level:
-#         print(i)
+
+def gen_diamonds():
+    for i in range(3):
+        coords = (randint(20 * 32, LEVEL_WIGHT - 20 * 32), randint(20 * 32, LEVEL_WIGHT - 20 * 32))
+        diamond = Diamond((coords[0], coords[1]))
+        crystal_group.add(diamond)
+        diamond_group.add(diamond)
+        all_sprites.add(diamond)
+
+
+def gen_emeralds():
+    for i in range(3):
+        coords = (randint(20 * 32, LEVEL_WIGHT - 20 * 32), randint(20 * 32, LEVEL_WIGHT - 20 * 32))
+        emerald = Emerald((coords[0], coords[1]))
+        crystal_group.add(emerald)
+        emeralds_group.add(emerald)
+        all_sprites.add(emerald)
+
